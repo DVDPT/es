12466d43a8a0621c4fb6abb8ff387f3983e86815
Ricardo Nunes <ricardomiguel.sn@gmail.com>
2014-01-08 16:06:44 +0000

    Added project controller

---
+++ code/SGPF/SGPF.DataController/IProjectController.cs
@@ -0,0 +1,29 @@
+﻿using SGPF.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGPF.DataController
+{
+    public interface IProjectController
+    {
+        Task<Project> Create(Project project);
+
+        Task<Project> GetById(int id);
+
+        Task SendToDispatchQueue(Project project);
+
+        Task Archive(Project project);
+
+        Task AddTechnicalOpinion(Project project, String comment, SGPF.Data.Manager.ManagerOpinion opinion);
+
+        Task AddDispatch(Project project, );
+
+        Task Suspend(Project project);
+
+        Task Resume(Project project);
+
+    }
+}
---
+++ code/SGPF/SGPF.DataController/ProjectController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGPF.DataController
+{
+	public class ProjectController : IProjectController
+	{
+        private readonly ISGPF
+	    public Task<Data.Project> Create(Data.Project project)
+        {
+	        throw new NotImplementedException();
+        }
+
+        public Task<Data.Project> GetById(int id)
+        {
+	        throw new NotImplementedException();
+        }
+
+        public Task SendToDispatchQueue(Data.Project project)
+        {
+	        throw new NotImplementedException();
+        }
+
+        public Task Archive(Data.Project project)
+        {
+	        throw new NotImplementedException();
+        }
+
+        public Task AddTechnicalOpinion(Data.Project project, ? ?)
+        {
+	        throw new NotImplementedException();
+        }
+
+        public Task AddDispatch(Data.Project project, ? ?)
+        {
+	        throw new NotImplementedException();
+        }
+
+        public Task Suspend(Data.Project project)
+        {
+	        throw new NotImplementedException();
+        }
+
+        public Task Resume(Data.Project project)
+        {
+	        throw new NotImplementedException();
+        }
+}
+}
